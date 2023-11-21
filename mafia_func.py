54f61908195477512cabc6813738b88a369b3c2b
alex <alexadr1803@gmail.com>
2023-11-21 16:53:46 +0300

    Version 2.2

--- mafia_func.py
+++ mafia_func.py
@@ -2,17 +2,16 @@
     Технические функции для игры в мафию
     Тут бы все оптимизировать, если кто шарит можете помочь?
 """
-
+import random
 from aiogram import types
 
-
 # Счетчики персонажей в игре
+from operator import attrgetter
+from collections import Counter
+
+
 def count_mafia(mafia_members):
-    k = 0
-    for i in list(mafia_members.keys()):
-        if mafia_members[i].role == 'Мафия':
-            k += 1
-    return k
+    return list(map(attrgetter('role'), mafia_members.values())).count('Мафия')
 
 
 def count_peace(mafia_members):
@@ -24,27 +23,15 @@ def count_peace(mafia_members):
 
 
 def count_lover(mafia_members):
-    k = 0
-    for i in list(mafia_members.keys()):
-        if mafia_members[i].role == 'Любовница':
-            k += 1
-    return k
+    return list(map(attrgetter('role'), mafia_members.values())).count('Любовница')
 
 
 def count_doctor(mafia_members):
-    k = 0
-    for i in list(mafia_members.keys()):
-        if mafia_members[i].role == 'Доктор':
-            k += 1
-    return k
+    return list(map(attrgetter('role'), mafia_members.values())).count('Доктор')
 
 
 def count_sheriff(mafia_members):
-    k = 0
-    for i in list(mafia_members.keys()):
-        if mafia_members[i].role == 'Шериф':
-            k += 1
-    return k
+    return list(map(attrgetter('role'), mafia_members.values())).count('Шериф')
 
 
 # Создает специальную клавиатуру для голосований с префиксом, чтобы отличать разные колбэки друг от друга
@@ -62,24 +49,14 @@ def create_prefix_keyboard(mafia_members: dict, prefix: str):
 
 # Возращает список с двумя значениями [Есть вердикт? : bool, Кто будет исключен? : int or None]
 def get_verdict(mafia_members: dict):
-    verdict = []
-    k = 0
-    votes = [None]
-    for i in mafia_members.keys():
-        verdict.append(mafia_members[i].vote_on_voting)
-    for i in list(set(verdict)):
-        g = verdict.count(i)
-        if g > k:
-            votes = [i]
-            k = g
-        elif g == k:
-            votes.append(i)
-    if len(votes) > 1:
-        return [False, None]
-    elif votes[0] is None:
-        return [True, None]
+    c = Counter(list(map(attrgetter('vote_on_voting'), mafia_members.values()))).most_common(2)
+    if len(c) > 1:
+        if c[0][1] == c[1][1]:
+            return [False, None]
+        else:
+            return [True, c[0][0]]
     else:
-        return [True, votes[0]]
+        return [True, c[0][0]]
 
 
 # Возращает список умерших игроков, соблюдая все условия (Доктор, Любовница, Житель не голосовал за свою роль?)
@@ -91,17 +68,18 @@ def get_killed_players(mafia_members: dict):
         if mafia_members[i].role == 'Мафия':
             if mafia_members[i].vote_on_role_voting is None:
                 killed_players.add(mafia_members[i].id)
+                mafias_voting.append(None)
             else:
                 mafias_voting.append(mafia_members[i].vote_on_role_voting)
-
     # Подведение итогов голосования мафии
-    k = 0
-    s = None
-    for i in list(set(mafias_voting)):
-        if mafias_voting.count(i) >= k:
-            k = mafias_voting.count(i)
-            s = i
-
+    c = Counter(mafias_voting).most_common(2)
+    if len(c) > 0:
+        if c[0][1] == c[1][1]:
+            s = random.choice([c[0][0], c[1][0]])
+        else:
+            s = c[0][0]
+    else:
+        s = c[0][0]
     # Хилл Доктором и приход Любовницы обрабатывают список убитых игроков
     doctors_heal = 0
     if s is not None:
