09df6f96f48e921b87f38d1eec3f25dc60ea3234
alex <alexadr1803@gmail.com>
2024-01-03 12:18:26 +0300

    Version 2.6 Hiper-Structure

--- moderator/moder_handlers.py
+++ moderator/moder_handlers.py
@@ -3,20 +3,24 @@ import re
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 from aiogram.enums import ParseMode
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import word_tokenize
 user_active_repeat = False
 router = Router()
-
-
-# Самописный фильтр, он так заебывает
+stemmer = SnowballStemmer("russian")
+# Фильтр матов
 class ChatCensureFilter(BaseFilter):
     async def __call__(self, msg: Message) -> bool:
         if msg.text is None:
             return False
         text = re.sub(r'[^\w\s]', ' ', msg.text.lower())
         with open("moderator/mats.txt", 'r', encoding='utf-8') as file:
-            if set([''.join(i.split('\n')) for i in file.readlines()]) & set(text.split()):
+            if set([stemmer.stem(word) for word in [''.join(i.split('\n')).lower() for i in file.readlines()]]) & set([stemmer.stem(word) for word in text.split()]):
+                print([stemmer.stem(word) for word in text.split()])
                 return True
             else:
+                print([stemmer.stem(word) for word in text.split()])
                 return False
 
 
